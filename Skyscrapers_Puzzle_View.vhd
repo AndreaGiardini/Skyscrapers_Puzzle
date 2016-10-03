dcb4b5083b632def60750e973ec3f4f15ab55c6b
Andrea Giardini <contact@andreagiardini.com>
2016-10-03 14:06:49 +0200

    Fix view numbers

--- Skyscrapers_Puzzle_View.vhd
+++ Skyscrapers_Puzzle_View.vhd
@@ -210,7 +210,7 @@ begin
                      end if;
 							
 							if (((pixel_x - pixel_x_start_pos) /= SPRITE_SIZE) and ((pixel_y - pixel_y_start_pos) /= SPRITE_SIZE)) then
-								case CONSTRAINTS(constraints_c,constraints_r) is
+								case MATRIX(constraints_c,constraints_r) is
 									when 0 => FB_COLOR <= zero_sprite(sprite_index);
 									when 1 => FB_COLOR <= one_sprite(sprite_index);
 									when 2 => FB_COLOR <= two_sprite(sprite_index);
