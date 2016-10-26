1d0dfe9f215dd597c0f17ebe38f05219cd66b7c2
Andrea Giardini <contact@andreagiardini.com>
2016-10-26 09:52:36 +0200

    Better keyboard logic

--- Skyscrapers_Puzzle_Controller.vhd
+++ Skyscrapers_Puzzle_Controller.vhd
@@ -28,77 +28,70 @@ entity Skyscrapers_Puzzle_Controller is
 end entity;
 
 architecture behavioral of Skyscrapers_Puzzle_Controller is
-	constant MOVEMENT_SPEED    : integer := 20;
+	constant MOVEMENT_SPEED    : integer := 15;
 	signal   time_to_next_move : integer range 0 to MOVEMENT_SPEED-1;
+	
+	constant keyRIGHT	: std_logic_vector(7 downto 0):=X"74";
+	constant keyLEFT	: std_logic_vector(7 downto 0):=X"6B";
+	constant keyUP 	: std_logic_vector(7 downto 0):=X"75";
+	constant keyDOWN 	: std_logic_vector(7 downto 0):=X"72";
+	constant key0		: std_logic_vector(7 downto 0):=X"45";
+	constant key1  	: std_logic_vector(7 downto 0):=X"16";
+	constant key2		: std_logic_vector(7 downto 0):=X"1E";
+	constant key3		: std_logic_vector(7 downto 0):=X"26";
+	constant key4		: std_logic_vector(7 downto 0):=X"25";
+	constant key5		: std_logic_vector(7 downto 0):=X"2E";
+	constant key6		: std_logic_vector(7 downto 0):=X"36";
+	constant key7		: std_logic_vector(7 downto 0):=X"3D";
+	constant key8		: std_logic_vector(7 downto 0):=X"3E";
+	constant key9		: std_logic_vector(7 downto 0):=X"46";
+	constant keyENTER	: std_logic_vector(7 downto 0):=X"5A";
 begin
 
-	TimedMove : process(CLOCK, RESET_N)
-		constant keyRIGHT	: std_logic_vector(7 downto 0):=X"74";
-		constant keyLEFT	: std_logic_vector(7 downto 0):=X"6B";
-		constant keyUP 	: std_logic_vector(7 downto 0):=X"75";
-		constant keyDOWN 	: std_logic_vector(7 downto 0):=X"72";
-		constant key0		: std_logic_vector(7 downto 0):=X"45";
-		constant key1  	: std_logic_vector(7 downto 0):=X"16";
-		constant key2		: std_logic_vector(7 downto 0):=X"1E";
-		constant key3		: std_logic_vector(7 downto 0):=X"26";
-		constant key4		: std_logic_vector(7 downto 0):=X"25";
-		constant key5		: std_logic_vector(7 downto 0):=X"2E";
-		constant key6		: std_logic_vector(7 downto 0):=X"36";
-		constant key7		: std_logic_vector(7 downto 0):=X"3D";
-		constant key8		: std_logic_vector(7 downto 0):=X"3E";
-		constant key9		: std_logic_vector(7 downto 0):=X"46";
-		constant keyENTER	: std_logic_vector(7 downto 0):=X"5A";
+	MainProcess : process
 	begin
+		wait until rising_edge(CLOCK);
+		MOVE_RIGHT <= '0';
+		MOVE_LEFT  <= '0';
+		MOVE_DOWN  <= '0';
+		MOVE_UP 	  <= '0';
+		NUMBER 	  <= "1111";
+		SOLVE		  <= '0';
+		REDRAW 	  <= '0';
+		if (TIME_10MS = '1') then
+			time_to_next_move  <= time_to_next_move - 1;
+		end if;
 		if (RESET_N = '0') then
 			time_to_next_move	<= 0;
-			MOVE_RIGHT 			<= '0';
-			MOVE_LEFT 			<= '0';
-			MOVE_DOWN 			<= '0';
-			MOVE_UP 				<= '0';
-			NUMBER 				<= "1111";
-			SOLVE		 			<= '0';
 			REDRAW 				<= '1';
-		elsif rising_edge(CLOCK) then
-			MOVE_RIGHT <= '0';
-			MOVE_LEFT  <= '0';
-			MOVE_DOWN  <= '0';
-			MOVE_UP 	  <= '0';
-			NUMBER 	  <= "1111";
-			SOLVE		  <= '0';
-			REDRAW 	  <= '0';			
-			if (TIME_10MS = '1') then
-				REDRAW <= '0';
-				if (time_to_next_move = 0) then
-					time_to_next_move  <= MOVEMENT_SPEED - 1;
-					case keyboardData is
-						when KEYRIGHT => if ( cursor_pos(1) < 3 ) then MOVE_RIGHT <= '1'; end if;
-						when KEYLEFT  => if ( cursor_pos(1) > 0 ) then MOVE_LEFT <= '1'; end if;
-						when KEYUP    => if ( cursor_pos(0) > 0 ) then MOVE_UP <= '1'; end if;
-						when KEYDOWN  => if ( cursor_pos(0) < 3 ) then MOVE_DOWN <= '1'; end if;
-						when KEY0 	  => NUMBER <= "0000";
-						when KEY1     => NUMBER <= "0001";
-						when KEY2 	  => NUMBER <= "0010";
-						when KEY3     => NUMBER <= "0011";
-						when KEY4     => NUMBER <= "0100";
-						when KEY5     => NUMBER <= "0101";
-						when KEY6     => NUMBER <= "0110";
-						when KEY7 	  => NUMBER <= "0111";
-						when KEY8     => NUMBER <= "1000";
-						when KEY9     => NUMBER <= "1001";
-						when KEYENTER => SOLVE  <= '1';
-						when others => -- do nothing
-							MOVE_RIGHT <= '0';
-							MOVE_LEFT  <= '0';
-							MOVE_DOWN  <= '0';
-							MOVE_UP    <= '0';
-							NUMBER 	  <= "1111";
-							SOLVE		  <= '0';
-					end case;
-					REDRAW <= '1';
-				else
-					time_to_next_move  <= time_to_next_move - 1;
-				end if;
-			end if;
+		end if;
+		if (time_to_next_move = 0) then
+			time_to_next_move  <= MOVEMENT_SPEED - 1;
+			case keyboardData is
+				when KEYRIGHT => if ( cursor_pos(1) < 3 ) then MOVE_RIGHT <= '1'; end if;
+				when KEYLEFT  => if ( cursor_pos(1) > 0 ) then MOVE_LEFT <= '1'; end if;
+				when KEYUP    => if ( cursor_pos(0) > 0 ) then MOVE_UP <= '1'; end if;
+				when KEYDOWN  => if ( cursor_pos(0) < 3 ) then MOVE_DOWN <= '1'; end if;
+				when KEY0 	  => NUMBER <= "0000";
+				when KEY1     => NUMBER <= "0001";
+				when KEY2 	  => NUMBER <= "0010";
+				when KEY3     => NUMBER <= "0011";
+				when KEY4     => NUMBER <= "0100";
+				when KEY5     => NUMBER <= "0101";
+				when KEY6     => NUMBER <= "0110";
+				when KEY7 	  => NUMBER <= "0111";
+				when KEY8     => NUMBER <= "1000";
+				when KEY9     => NUMBER <= "1001";
+				when KEYENTER => SOLVE  <= '1';
+				when others => -- do nothing
+					MOVE_RIGHT <= '0';
+					MOVE_LEFT  <= '0';
+					MOVE_DOWN  <= '0';
+					MOVE_UP    <= '0';
+					NUMBER 	  <= "1111";
+					SOLVE		  <= '0';
+			end case;
+			REDRAW <= '1';
 		end if;
 	end process;
 
