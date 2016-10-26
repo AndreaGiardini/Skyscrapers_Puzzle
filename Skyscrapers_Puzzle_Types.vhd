587eaa8801ee8a2bbf79a13300b3a7438009c874
Andrea Giardini <contact@andreagiardini.com>
2016-10-26 17:49:15 +0200

    Revert "Revert "Optimized integer sizes""
    This reverts commit d5dee865d90bf3954b61b9df3569734bb5b6a592.

--- Skyscrapers_Puzzle_Types.vhd
+++ Skyscrapers_Puzzle_Types.vhd
@@ -4,13 +4,13 @@ use ieee.std_logic_1164.all;
 
 package Skyscrapers_Puzzle_Types is
 	type SOLUTIONS_TYPE		is array(0 to 3, 0 to 3, 0 to 3) of std_logic;	-- Possible solutions
-	type ROW_TYPE				is array(0 to 3) of integer;
+	type ROW_TYPE				is array(0 to 3) of integer range 0 to 4;
 --	type CELL_SOLUTIONS		is array(integer range<>) of integer;
 --	type SOLUTIONS_TYPE		is array(3 downto 0, 3 downto 0) of CELL_SOLUTIONS;
-	type MATRIX_TYPE			is array(0 to 3, 0 to 3) of integer; -- (rows, columns)
-	type CONSTRAINTS_TYPE	is array(0 to 3, 0 to 3) of integer; -- Index 0: LEFT, Index 1: TOP, Index 2: BOTTOM, Index 3: RIGHT
+	type MATRIX_TYPE			is array(0 to 3, 0 to 3) of integer range 0 to 4; -- (rows, columns)
+	type CONSTRAINTS_TYPE	is array(0 to 3, 0 to 3) of integer range 0 to 4; -- Index 0: LEFT, Index 1: TOP, Index 2: BOTTOM, Index 3: RIGHT
 	type SCHEMAS_TYPE			is array(0 to 4) of CONSTRAINTS_TYPE;
-	type CURSOR_POS_TYPE		is array(1 downto 0) of integer;					-- (rows, columns)
+	type CURSOR_POS_TYPE		is array(1 downto 0) of integer range 0 to 3;					-- (rows, columns)
 	
 	constant  SPRITE_SIZE    : integer        := 45;
 end package;
